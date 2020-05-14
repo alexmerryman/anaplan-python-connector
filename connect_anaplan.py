f8497775fee35440d67492d3d667dbc0e04498d8
Alexander Merryman <merryman.alexander@gmail.com>
2020-05-14 11:39:32 -0600

    - Helper functions - Overall connect script

--- connect_anaplan.py
+++ connect_anaplan.py
@@ -9,7 +9,7 @@
 #user = 'Basic ' + str(base64.b64encode((f'{username}:{password}').encode('utf-8')).decode('utf-8'))
 #    return("Hello, World!")`
 
-
+import json
 import anaplan_connect_helper_functions
 
 # Insert your workspace Guid
@@ -40,10 +40,11 @@ postHeaders = {
 print('------------------- WORKSPACES -------------------')
 try:
     workspaces = anaplan_connect_helper_functions.get_workspaces(basic_auth_user)
+    workspaces_data = json.loads(workspaces.text)
 except:
     print('ERROR: Unable to get workspaces.')
 if workspaces.status_code == 200:
-    print(workspaces.text)
+    print(workspaces_data)
 else:
     print('Error: Status Code {}'.format(workspaces.status_code))
 
@@ -51,10 +52,11 @@ else:
 print('------------------- MODEL INFO -------------------')
 try:
     model_info = anaplan_connect_helper_functions.get_model_info(mGuid, basic_auth_user)
+    model_info_data = json.loads(model_info.text)
 except:
     print('ERROR: Unable to get model info.')
 if model_info.status_code == 200:
-    print(model_info.text)
+    print(model_info_data)
 else:
     print('Error: Status Code {}'.format(model_info.status_code))
 
@@ -62,33 +64,39 @@ else:
 print('------------------- MODEL FILES -------------------')
 try:
     model_files = anaplan_connect_helper_functions.get_model_files(wGuid, mGuid, basic_auth_user)
+    model_files_data = json.loads(model_files.text)
 except:
     print('ERROR: Unable to get model files.')
 if model_files.status_code == 200:
-    print(model_files.text)
+    print(model_files_data)
 else:
     print('Error: Status Code {}'.format(model_files.status_code))
 
 
 print('------------------- FILE INFO -------------------')
-fileID = '116000000000'
-try:
-    chunk_data = anaplan_connect_helper_functions.get_chunk_data(wGuid, mGuid, fileID, basic_auth_user)
-except:
-    print('ERROR: Unable to get model files.')
-if chunk_data.status_code == 200:
-    print(chunk_data.text)
-else:
-    print('Error: Status Code {}'.format(chunk_data.status_code))
+# file_id = '116000000000'
+for f in model_files_data:
+    file_id = f['id']
+    try:
+        chunk_data = anaplan_connect_helper_functions.get_chunk_data(wGuid, mGuid, file_id, basic_auth_user)
+        chunk_data_data = json.loads(chunk_data.text)
+    except:
+        print('ERROR: Unable to get model files.')
+    if chunk_data.status_code == 200:
+        print('FILE ID:', file_id)
+        print(chunk_data_data)
+    else:
+        print('Error: Status Code {}'.format(chunk_data.status_code))
 
 
 print('------------------- MODEL IMPORTS -------------------')
 try:
     model_imports = anaplan_connect_helper_functions.get_imports(wGuid, mGuid, basic_auth_user)
+    model_imports_data = json.loads(model_imports.text)
 except:
     print('ERROR: Unable to get model files.')
 if model_imports.status_code == 200:
-    print(model_imports.text)
+    print(model_imports_data)
 else:
     print('Error: Status Code {}'.format(model_imports.status_code))
 
@@ -96,10 +104,11 @@ else:
 print('------------------- MODEL EXPORTS -------------------')
 try:
     model_exports = anaplan_connect_helper_functions.get_exports(wGuid, mGuid, basic_auth_user)
+    model_exports_data = json.loads(model_exports.text)
 except:
     print('ERROR: Unable to get model files.')
 if model_exports.status_code == 200:
-    print(model_exports.text)
+    print(model_exports_data)
 else:
     print('Error: Status Code {}'.format(model_exports.status_code))
 
@@ -107,10 +116,11 @@ else:
 print('------------------- MODEL ACTIONS -------------------')
 try:
     model_actions = anaplan_connect_helper_functions.get_actions(wGuid, mGuid, basic_auth_user)
+    model_actions_data = json.loads(model_exports.text)
 except:
     print('ERROR: Unable to get model files.')
 if model_actions.status_code == 200:
-    print(model_actions.text)
+    print(model_actions_data)
 else:
     print('Error: Status Code {}'.format(model_actions.status_code))
 
@@ -118,12 +128,14 @@ else:
 print('------------------- MODEL PROCESSES -------------------')
 try:
     model_processes = anaplan_connect_helper_functions.get_processes(wGuid, mGuid, basic_auth_user)
+    model_processes_data = json.loads(model_processes.text)
 except:
     print('ERROR: Unable to get model files.')
 if model_processes.status_code == 200:
-    print(model_processes.text)
+    print(model_processes_data)
 else:
     print('Error: Status Code {}'.format(model_processes.status_code))
+
 
 # # Runs an export request, and returns task metadata to 'postExport.json'
 # try:
