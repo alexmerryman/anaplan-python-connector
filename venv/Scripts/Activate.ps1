89fd0ed4e9f95a671d2accdf2d2b24677a7e0f47
Elvin Baghele <Elvin.Baghele@tli-ind.com>
2020-07-15 20:39:30 +0530

    elvin changes

--- venv/Scripts/Activate.ps1
+++ venv/Scripts/Activate.ps1
@@ -27,7 +27,7 @@ function global:deactivate ([switch]$NonDestructive) {
 
 deactivate -nondestructive
 
-$env:VIRTUAL_ENV="C:\Users\Alexander Merryman\PycharmProjects\covid-modeling-anaplan\venv"
+$env:VIRTUAL_ENV="E:\D Drive\Learning Initiative\anaplan connector disabled\anaplan-python-connector\venv"
 
 if (! $env:VIRTUAL_ENV_DISABLE_PROMPT) {
     # Set the prompt to include the env name
--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\Alexander Merryman\PycharmProjects\covid-modeling-anaplan\venv
+set VIRTUAL_ENV=E:\D Drive\Learning Initiative\anaplan connector disabled\anaplan-python-connector\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
